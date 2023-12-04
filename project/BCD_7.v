05bfcd0e52993e9365a93311190e542ddeb1e4f0
Gerardo Cerpa <81593072+Gcerpa01@users.noreply.github.com>
2023-12-04 01:16:00 -0800

    Update BCD_7.v

--- project/BCD_7.v
+++ project/BCD_7.v
@@ -1,43 +1,98 @@
-module BCD_7 #(parameter WL = 4)(input [WL-1:0] value,
+module BCD_7 #(parameter DL = 4)
+            (input [DL-1:0] ones,
+            input [DL-1:0] ten,
+            input [DL-1:0] hund,
+            input CLK,
+            input RST,
+            output reg [3:0] anode,
             output reg[6:0] OUT);
 
-always @(*) begin
-    case (value)
-        0: OUT = 7'b0000001;
+        parameter NULL = 7'b1111111;
+        parameter ZERO = 7'b0000001;
+        parameter ONE = 7'b1001111;
+        parameter TWO = 7'b0010010;
+        parameter THREE = 7'b0000110;
+        parameter FOUR = 7'b1001100;
+        parameter FIVE = 7'b0100100;
+        parameter SIX = 7'b0100000;
+        parameter SEVEN = 7'b0001111;
+        parameter EIGHT = 7'b0000000;
+        parameter NINE = 7'b0000100;
 
-        1: OUT = 7'b1001111;
 
-        2: OUT = 7'b0010010;
+        reg[1:0] anode_select;
+        reg[16:0] anode_timer;
 
-        3: OUT = 7'b0000110;
+        //go through anodes
+        always @ (posedge CLK or posedge RST) begin
+            if(RST) begin
+                anode_select <= 0;
+                anode_select <= 0;
+            end
+            else begin
+                if(anode_timer == 99_999) begin
+                    anode_timer <= 0;
+                    if(anode_select == 2) anode_select <= 0;
+                    else anode_select <= anode_select + 1;
+                end
+                else anode_timer <= anode_timer + 1;
+            end
+        end
 
-        4: OUT = 7'b1001100;
+        always @(anode_select) begin
+            case(anode_select) 
+                0: anode = 4'b1110;
+                1: anode = 4'b1101;
+                2: anode = 4'b1011;
+            endcase
+        end
 
-        5: OUT = 7'b0100100;
 
-        6: OUT = 7'b0100000;
-
-        7: OUT = 7'b0001111;
-
-        8: OUT = 7'b0000000;
-
-        9: OUT = 7'b0000100;
-
-        10: OUT = 7'b0001000;
-
-        11: OUT = 7'b1100000;
-        
-        12: OUT = 7'b0110001;
-
-        13: OUT = 7'b1000010;
-
-        14: OUT = 7'b0110000;
-
-        15: OUT = 7'b0111000;
-
-        default: OUT = 7'b0111000;
-    endcase
-end
+        always @* begin
+            case(anode_select)
+                0: begin
+                    case(ones)
+                        0: OUT = ZERO;
+                        1: OUT = ONE;
+                        2: OUT = TWO;
+                        3: OUT = THREE;
+                        4: OUT = FOUR;
+                        5: OUT = FIVE;
+                        6: OUT = SIX;
+                        7: OUT = SEVEN;
+                        8: OUT = EIGHT;
+                        9: OUT = NINE;
+                    endcase
+                end
+                1: begin
+                    case(ten)
+//                        0: OUT = ZERO;
+                        0:begin
+                            if(hund != 0) OUT = ZERO;
+                            else OUT = NULL;
+                        end
+                        1: OUT = ONE;
+                        2: OUT = TWO;
+                        3: OUT = THREE;
+                        4: OUT = FOUR;
+                        5: OUT = FIVE;
+                        6: OUT = SIX;
+                        7: OUT = SEVEN;
+                        8: OUT = EIGHT;
+                        9: OUT = NINE;
+                    endcase 
+                end
+                
+                2: begin
+                    case(hund)
+//                        0: OUT = ZERO;
+                        0: OUT = NULL;
+                        1: OUT = ONE;
+                        2: OUT = TWO;
+                    endcase
+                end
+            endcase
+        end
 
 
 endmodule
