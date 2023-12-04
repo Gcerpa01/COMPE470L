0bfe4dc76ed28ee922122a46ad3c8819c3fbd853
Gerardo Cerpa <81593072+Gcerpa01@users.noreply.github.com>
2023-12-04 01:16:41 -0800

    Update FIFO_SOLO_TEST.v

--- project/FIFO_SOLO_TEST.v
+++ project/FIFO_SOLO_TEST.v
@@ -1,10 +1,11 @@
+
 module FIFO_SOLO_TEST(
     input CLK_IN,
-    input [3:0] sw,      // data_in
-    input btn1,         // btn for Reset
-    input btn2,         // btn for write
-    input btn3,         // btn for read
-    output wire [3:0] anode, // common anode
+    input [7:0] sw,        // 8-bit data_in
+    input btn1,            // btn for Reset
+    input btn2,            // btn for write
+    input btn3,            // btn for read
+    output wire [3:0] anode,// common anode
     output wire dp,
     output wire [6:0] BCD_out, // 7 segment
     output wire full,
@@ -13,16 +14,26 @@ module FIFO_SOLO_TEST(
     output wire seconds
 );
 
-    parameter WL = 4;
+///////// FIFO purposes /////////////////////
+    parameter WL = 8;
+    parameter DL = 4; //for display purposes
     parameter DEPTH = 4;
+    reg [7:0] din; // Updated to 8 bits for the switch value
+//////////////////////////////////////////
 
-    assign anode = 4'b1110;
+
     assign dp = 1;
 
+////// DISPLAY PURPOSES /////////////////////
     wire [WL - 1 :0] dout;
+    wire [DL-1:0] ones;
+    wire [DL-1:0] ten;
+    wire [DL-1:0] hund;
+//////////////////////////////////////////
+
+
     reg [25:0] count = 0;
     reg CLK_OUT;
-    reg [3:0] din; // New register to hold the value of sw
 
     reg second = 0;
 
@@ -42,23 +53,25 @@ module FIFO_SOLO_TEST(
 
     FIFO #(.WL(WL),
            .DEPTH(DEPTH))
-        DUT(.CLK(CLK_OUT),
+        DUT0(.CLK(CLK_OUT),
             .RST(btn1),
             .wReq(btn2),
             .rReq(btn3),
-            .din(din),  // Use the new din register
+            .din(din),
             .dout(dout),
             .empty(empty),
             .full(full),
             .error(error));
-
-    BCD_7  #(.WL(WL)) 
-           DUT0(.value(dout), 
-           .OUT(BCD_out)); // show on display
-
+                  
+    BCD_7 #(.DL(DL)) DUT2(.ones(ones),.ten(ten),.hund(hund),.CLK(CLK_IN),.RST(btn1),.anode(anode),.OUT(BCD_out)); // show on display
+      
     // Assign the value of sw to din
     always @(posedge CLK_IN) begin
-            din <= sw;
+        din <= sw;
     end
+    
+    assign ones = dout % 10;
+    assign ten = (dout/10) % 10;
+    assign hund = (dout/100);
 
 endmodule
